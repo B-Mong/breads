e723408a95ad52e13ca780fd6d64744f1873e4a4
Brian Mong <brian.mong9@gmail.com>
2022-07-15 19:57:13 -0700

    Created EDIT route and view

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -19,11 +19,18 @@ breads.get('/new', (req, res) => {
     res.render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+    res.render('edit', {
+        bread: Bread[req.params.indexArray],
+        index: req.params.indexArray
+    })
+})
 
 // Show // Gets data and display it on the Show.jsx html template. 
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
-        res.render('Show', {
+        res.render('show', {
             bread: Bread[req.params.arrayIndex],
             index: req.params.arrayIndex,
         })
@@ -51,5 +58,16 @@ breads.post('/', (req, res) => {
 breads.delete('/:indexArray', (req, res) => {
     Bread.splice(req.param.indexArray, 1)
     res.status(303).redirect('/breads')
+})
+
+// UPDATE 
+breads.put('/:arrayIndex', (req, res) => {
+    if(req.body.hasGluten === 'on'){
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+    Bread[req.params.arrayIndex] = req.body
+    res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 module.exports = breads
